ef4ce9ea69ecaa30624e282dfb20fdda1b24dff6
Eden Fernando <eden@eandshome.net>
2026-04-21 16:04:11 +1000

    Initial commit

---
+++ Group5/Group5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Group5
+//
+//  Created by Eden Fernando on 21/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Group5/Group5/Group5App.swift
@@ -0,0 +1,17 @@
+//
+//  Group5App.swift
+//  Group5
+//
+//  Created by Eden Fernando on 21/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct Group5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
