73eb73f984fd4ffb5ce7bcbb85558cbac794dbc1
Eden Fernando <eden@eandshome.net>
2026-05-02 13:40:47 +1000

    colour

--- Group5/Group5/Constants.swift
+++ Group5/Group5/Constants.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
+
+// Stores custom colours that can be used throughout the application
+enum AppColours {
+    static let yellow = Color(red: 245/255, green: 245/255, blue: 245/255)
+    static let orange = Color(red: 255/255, green: 173/255, blue: 123/255)
+    static let black = Color.black
+    static let white = Color.white
+}
